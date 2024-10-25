f05bf5c1efba352198f8837f4f5f7109ef758ca0
Basit ali <basitalyshah51214@gmail.com>
2024-10-25 05:57:42 -0700

    changes language in the signup or login screen

--- lib/main.dart
+++ lib/main.dart
@@ -46,8 +46,8 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  // Stripe.publishableKey =
-  //     "pk_test_51PlW60P8NBIKG6QO0dHfDou7Jlnpx3KPGruG9S4JTX4PQfgFyjIIlKXmEuoSO1e5ksahBTuSM0Hdk4cKbgYR7YvM00awJvQ8KQ";
+  Stripe.publishableKey =
+      "pk_test_51PlW60P8NBIKG6QO0dHfDou7Jlnpx3KPGruG9S4JTX4PQfgFyjIIlKXmEuoSO1e5ksahBTuSM0Hdk4cKbgYR7YvM00awJvQ8KQ";
   Stripe.instance.applySettings();
   runApp(const MyApp());
 }
